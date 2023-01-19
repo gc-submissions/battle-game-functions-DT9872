407a41877f57e3177d61f7ffe3e00a21fb568392
Dana Tanner <nielsen.danatanner@gmail.com>
2023-01-19 08:31:41 -0500

    update

--- src/script.js
+++ src/script.js
@@ -30,22 +30,21 @@ const isDead = (health) => {
 function fight(player1, player2, player1Health, player2Health) {
   while (true) {
     let attacker = chooseOption(player1, player2);
-        if (attacker === player1) {
-          player2Health = attackPlayer(player2Health);
-          logHealth(player2, player2Health);
-          if (isDead(player2Health)) {
-            logDeath(player1, player2);
-            break;
-          }
-        }
-       }  else {
-          player1Health = attackPlayer(player1Health);
-          logHealth(player1, player1Health);
-          if (isDead(player1Health)) {
-          logDeath(player2, player1);
-          break;
-          }
-        }
+    if (attacker === player1) {
+      player2Health = attackPlayer(player2Health);
+      logHealth(player2, player2Health);
+      if (isDead(player2Health)) {
+        logDeath(player1, player2);
+        break;
+      }
+    } else {
+      player1Health = attackPlayer(player1Health);
+      logHealth(player1, player1Health);
+      if (isDead(player1Health)) {
+        logDeath(player2, player1);
+        break;
+      }
+    }
   }
 }
 // fight("Adam", "Mitch", 100, 100);
